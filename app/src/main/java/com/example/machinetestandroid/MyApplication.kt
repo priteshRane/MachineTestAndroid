a5f0d7975d866a0b9d3ebf223466f8d7b72cebb5
priteshRane <priteshrane01@gmail.com>
2020-12-10 22:57:58 +0530

    Dagger migrate to Hilt

--- app/src/main/java/com/example/machinetestandroid/MyApplication.kt
+++ app/src/main/java/com/example/machinetestandroid/MyApplication.kt
@@ -1,11 +1,4 @@
 package com.example.machinetestandroid
 
-import android.app.Application
-import com.example.machinetestandroid.di.AppComponent
-import com.example.machinetestandroid.di.DaggerAppComponent
-
-open class MyApplication: Application() {
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.factory().create(applicationContext)
-    }
+class MyApplication {
 }
---
+++ app/src/main/java/com/example/machinetestandroid/di/NetworkConnectionInterceptorModule.kt
@@ -0,0 +1,4 @@
+package com.example.machinetestandroid.di
+
+class NetworkConnectionInterceptorModule {
+}
